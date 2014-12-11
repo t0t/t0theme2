010e7996058dbd1c5e42258303588697f0401532
Sergio Forés <sergiofores@gmail.com>
2014-12-11 03:26:46 +0100

    Ajustes

--- template-cpt1.php
+++ template-cpt1.php
@@ -5,89 +5,17 @@ Template Name: CPT
 ?>
 <?php get_header(); ?>
 
-<main>
+<main role="main">
 
+	<!-- Nav -->
  	<?php get_template_part('templates/nav', 'main' ); ?>
 
-    <section role="main">
+	<!-- Header -->
+    <?php get_template_part('templates/page', 'header'); ?>
 
+    <!-- Content -->
     <?php get_template_part('templates/content', 'cpt'); ?>
-
-    </section>
 
 </main>
 
 <?php get_footer(); ?>
-
-
-
-
-
-
-
-<section>
-
-	<?php 
-		$this_post = $post->ID;
-		$loop = new WP_Query( array( 'post_type' => 'cpt', 'post__not_in' => array($this_post), 'category_name' => '' )); 
-	?>	
-
-	<dl>
-		<dt>Listado de <?php the_title() ?></dt>
-				
-		<?php while ($loop->have_posts()) : $loop->the_post(); ?>
-	
-		<dd class="icon-tick">
-		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-		<?php the_title(); ?></a>
-		</dd>
-				
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); ?>
-	</dl>
-	
-</section>
-
-
-<section>
-
-	<?php 
-	$this_post = $post->ID;
-	$loop = new WP_Query( array( 
-							'post_type' => 'cpt',
-							'post__not_in' => array($this_post),
-							'category_name' => '' 
-							)); 
-	?>
-	
-	<div class="jcarousel">
-
-		<ul>
-			<?php while ($loop->have_posts()) : $loop->the_post(); ?>
-			<li>
-				<? if ( has_post_thumbnail() ) { ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail('medium'); ?>
-				</a>
-				<?}else {?>
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Alt" class="logo-img">
-				<?}?>
-				<h3>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><? the_title(); ?></a>
-				</h3>
-				<?php //the_excerpt(); ?>
-			</li>
-				
-			<?php endwhile; ?>
-			<?php wp_reset_postdata(); ?>
-		</ul>
-
-	</div>
-
-	<!-- Prev/next controls -->
-	<a href="#" class="btn btn--slider jcarousel-control-prev">
-	<i class="icon-arrow-left"></i></a>
-	<a href="#" class="btn btn--slider jcarousel-control-next">
-	<i class="icon-arrow-right"></i></a>
-
-</section>
--- templates/content-cpt.php
+++ templates/content-cpt.php
@@ -1,102 +1,41 @@
 <div class="main__content">
     
-    <!-- Header -->
-    <?php get_template_part('templates/page', 'header'); ?>
-    
-
-    <!-- Carousel -->
-    <section class="jcarousel">
-
-    <?php 
-    $this_post = $post->ID;
-    $loop = new WP_Query( array( 
-        'post_type' => 'cpt',
-        'post__not_in' => array($this_post),
-        'category_name' => '') ); 
-    ?>
-
-        <ul>
-            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-                                
-                <li>
-                <? if ( has_post_thumbnail() ) { ?>
-                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                </a>
-                <?}else {?>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="t0theme" class="logo-img">
-                <?}?>
-
-                    <h3>
-                        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                        <? the_title(); ?>
-                        </a>
-                    </h3>
-                </li>
-                
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
-
-        </ul>
-
-    </div>
-
-    <!-- Prev/next controls -->
-    <a href="#" class="jcarousel-control-prev btn btn--slider">
-    <span class="icon-arrow-left"></span></a>
-    <a href="#" class="jcarousel-control-next btn btn--slider">
-    <span class="icon-arrow-right"></span></a>
-
-    </section>
-
-
-    
     <section class="main__content__section">
+
     <?php 
     $this_post = $post->ID;
     $loop = new WP_Query( array( 
-        'post_type' => 'cpt',
-        'post__not_in' => array($this_post),
-        'category_name' => '') ); 
+    'post_type' => 'cpt',
+    'posts_per_page' => '',
+    'post__not_in' => array($this_post),
+    'category_name' => '') ); 
     ?>
-            
+
         <!-- post -->
-        <article class="article-post">
+        <article class="grid">
 
-            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+        <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+        
+            <div class="grid__item">
 
-            <ul>
-            <li>
-                <? if ( has_post_thumbnail() ) { ?>
-                <figure class="article-post__figure">
-                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                        <?php the_post_thumbnail('thumbnail'); ?>
-                </a>
-                <?}else {?>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="t0theme" class="logo-img">
-                <?}?>
-                    <figcaption>
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><? the_title(); ?></a>
-                    </figcaption>
-                </figure>
-            </li>
-            </ul>
+            <? if ( has_post_thumbnail() ) { ?>
+            <figure>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('thumbnail'); ?>
+                <figcaption><?php the_title(); ?></figcaption></a>
+            </figure>
+            <?}else {?>   
+            <figure>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="" class="">
+                <figcaption><?php the_title(); ?></figcaption>
+            </figure>
+            <?}?>
 
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
+            </div>
             
+        <?php endwhile; ?>
+
         </article>
 
     </section>
-
-        <?php
-            if ( is_home() ) :
-            get_sidebar();
-            elseif ( is_404() ) :
-            get_sidebar( '404' );
-            else :
-            get_sidebar( 'userpicture' );
-            endif;
-        ?>
 
 </div>
